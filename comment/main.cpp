e2d7dab56ba5481f17e2e5900e94172db988c0a7
rahatdev99 <rahatdev99@gmail.com>
2023-01-03 21:38:55 +0600

    comment use program

---
+++ comment/main.cpp
@@ -0,0 +1,20 @@
+//one line comment
+
+//This brings in the iostream library
+#include <iostream>
+
+/*
+  This is a block comment that englobest multiple lines of text
+*/
+
+/*
+  This is multiple lines comment
+*/
+
+int main() {
+    //This is going to print "Hello world!" to the console
+    std::cout<<"Hello world!"<<std::endl;
+
+    return 0;
+    //Program ends hear
+}
