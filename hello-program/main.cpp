3eca0fa2c54904a100923f5796cdad73757b414d
rahatdev99 <rahatdev99@gmail.com>
2023-01-03 21:36:14 +0600

    Hello world program

---
+++ hello-program/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+
+    std::cout<<"Hello world -> 1"<<std::endl;
+    std::cout<<"Hello world -> 2"<<std::endl;
+    std::cout<<"Hello world -> 3"<<std::endl;
+    
+    return 0;
+}
