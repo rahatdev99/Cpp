75ef1bdbf7a2d984f423b0773332eda87c603e14
rahatdev99 <rahatdev99@gmail.com>
2023-01-05 11:27:42 +0600

    NumberSystems

--- NumberSystems/main.cpp
+++ NumberSystems/main.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
 
-
 int main() {
     
+    int number1 = 15; // Decimal
+    int number2 = 017; // Octal
+    int number3 = 0x0F; // Hexadecimal
+    int number4 = 0b00001111; // Binary
+
+    std::cout<<"Number1: "<<number1<<std::endl;
+    std::cout<<"Number2: "<<number2<<std::endl;
+    std::cout<<"Number3: "<<number3<<std::endl;
+    std::cout<<"Number4: "<<number4<<std::endl;
+
     return 0;
 }
