2c833dd5424814e1555bf0a803116284427ddda0
rahatdev99 <rahatdev99@gmail.com>
2023-01-10 12:34:09 +0600

    chearacter

--- 2. Variables And Data Types/CharactersAndText/main.cpp
+++ 2. Variables And Data Types/CharactersAndText/main.cpp
@@ -2,5 +2,26 @@
 
 int main() {
     
+    char character1 {'a'};
+    char character2 {'r'};
+    char character3 {'r'};
+    char character4 {'o'};
+    char character5 {'w'};
+
+    std::cout<<character1<<std::endl;
+    std::cout<<character2<<std::endl;
+    std::cout<<character3<<std::endl;
+    std::cout<<character4<<std::endl;
+    std::cout<<character5<<std::endl;
+
+    //One byte in memory: 2^8 = 256 defferent value (0 ~ 255)
+    std::cout<<std::endl;
+
+    char value = 65; //ASCII character code for 'A'
+
+    std::cout<<"Value: "<<value<<std::endl;
+    std::cout<<"Value(int): "<<static_cast <int> (value) <<std::endl;
+
+
     return 0;
 }
